9282a7d2814ed63188cc7a6cb686e772ccf03557
Martin Hungendorfer <e11904212@student.tuwien.ac.at>
2023-02-08 14:22:55 +0100

    Revert "chore: changed url of prod backend"
    This reverts commit 1bc1676a700565db39a8f964c1a3c8ad573cc729.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "https://pcc-demo-api.duckdns.org/v1",
+  apiUrl: "https://api.pcc-demo.duckdns.org/v1",
   defaultPolygon: "POLYGON ((15.3478831 48.2094401, 15.3466774 48.2135189, 15.3433063 48.2126641, 15.3443565 48.2091379, 15.3478831 48.2094401))",
   defaultDatetimeFrom: "2022-08-01T00:00:00.000Z",
   defaultDatetimeTo: "2022-09-30T00:00:00.000Z",
